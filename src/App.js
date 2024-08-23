21d621c823a4f6fed35348484d2608540ebc04e6
Deven Pawar <devenpawar8@gmail.com>
2024-08-23 20:46:03 +0530

    Second commit

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
   const formatTime = (time) => {
     const getSeconds = `0${time % 60}`.slice(-2);
     const minutes = `${Math.floor(time / 60)}`;
-    const getMinutes = `0${minutes % 60}`.slice(-2);
+    const getMinutes = `0${minutes % 60}`.slice(-1);
 
     return `${getMinutes} : ${getSeconds}`;
   };
